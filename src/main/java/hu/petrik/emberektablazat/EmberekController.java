7f573f44d3d559d3f9a82f3b5039d8f113345ada
Morgován Nimród <morgovannimrod22@gmail.com>
2022-10-27 11:37:19 +0200

    Feladat elkészítése

--- src/main/java/hu/petrik/emberektablazat/EmberekController.java
+++ src/main/java/hu/petrik/emberektablazat/EmberekController.java
@@ -1,9 +1,7 @@
 package hu.petrik.emberektablazat;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class EmberekController {
@@ -14,17 +12,41 @@ public class EmberekController {
     private TableColumn<Ember, String> korOszlop;
     @FXML
     private TableColumn<Ember, Integer> nevOszlop;
+    @FXML
+    private Spinner korMezo;
+    @FXML
+    private TextField nevMezo;
 
     @FXML
     private void initialize() {
+        initSpinner();
         this.nevOszlop.setCellValueFactory(new PropertyValueFactory<>("nev"));
         this.korOszlop.setCellValueFactory(new PropertyValueFactory<>("eletkor"));
-        Ember e1 = new Ember("Gipsz Jakab",25);
-        Ember e2 = new Ember("Teszt Elek",52);
-        Ember e3 = new Ember("Szabolcsi Károly",11);
+        Ember e1 = new Ember("Gipsz Jakab", 25);
+        Ember e2 = new Ember("Teszt Elek", 52);
+        Ember e3 = new Ember("Szabolcsi Károly", 11);
         this.emberek.getItems().add(e1);
         this.emberek.getItems().add(e2);
         this.emberek.getItems().add(e3);
+
+    }
+
+    @FXML
+    private void initSpinner() {
+        this.korMezo.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 120));
+    }
+
+    @FXML
+    private void hozzaadClick() {
+        if (!this.nevMezo.getText().isEmpty()) {
+            Ember e = new Ember(this.nevMezo.getText(), (int) this.korMezo.getValueFactory().getValue());
+            this.emberek.getItems().add(e);
+        } else {
+            Alert felugroAblak = new Alert(Alert.AlertType.WARNING);
+            felugroAblak.setHeaderText("Figyelmeztetés");
+            felugroAblak.setContentText("Kérem adja meg a nevét");
+            felugroAblak.show();
+        }
     }
 
 }
